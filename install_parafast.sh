0a2c2cf4cfea07d5f539e5ee8aed8cbeb7060a7c
parascripts <chamanola0@gmail.com>
2025-07-22 00:55:01 +0530

    Add files via upload

---
+++ install_parafast.sh
@@ -0,0 +1,22 @@
+ARCH=$(uname -m)
+
+URL32="https://github.com/chamanola/parafast/raw/main/android%2032%20bit/parafast"
+URL64="https://github.com/chamanola/parafast/raw/main/android%2Blinux%2064bit/parafast"
+
+if [[ "$ARCH" == "armv7l" || "$ARCH" == "i686" || "$ARCH" == "x86" || "$ARCH" == "arm" ]]; then
+    echo "32-bit architecture detected (armv7, i686, x86, or arm). Installing 32-bit Parafast..."
+    DOWNLOAD_URL=$URL32
+elif [[ "$ARCH" == "arm64" || "$ARCH" == "aarch64" || "$ARCH" == "x86_64" ]]; then
+    echo "64-bit architecture detected (arm64, aarch64, x86_64). Installing 64-bit Parafast..."
+    DOWNLOAD_URL=$URL64
+else
+    echo "⚠️ Unsupported architecture detected: $ARCH."
+    exit 1
+fi
+
+mkdir -p ~/go/bin
+curl -Lo ~/go/bin/parafast $DOWNLOAD_URL && chmod +x ~/go/bin/parafast
+
+echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc && source ~/.bashrc
+
+echo -e "\n✅ Parafast installation successful! You can now run 'parafast' command."
